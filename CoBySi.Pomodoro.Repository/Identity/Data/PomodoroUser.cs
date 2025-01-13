74f67f1a210c8d64654171bafbd1283776e51f0c
Simon Adrell <simon.adrell@gmail.com>
2025-01-13 20:24:36 +0100

    Add account management pages and layouts for user authentication

---
+++ CoBySi.Pomodoro.Repository/Identity/Data/PomodoroUser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CoBySi.Pomodoro.Repository.Identity.Data;
+
+// Add profile data for application users by adding properties to the PomodoroUser class
+public class PomodoroUser : IdentityUser
+{
+}
+
