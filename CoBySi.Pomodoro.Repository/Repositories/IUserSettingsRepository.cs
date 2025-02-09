194132f0a00eec23ce74f0643c7903a58c6eec5c
Simon Adrell <simon.adrell@gmail.com>
2025-02-09 13:13:31 +0100

    Notification-settings (#9)
    * Add notification settings management and repository methods
    
    * Saving notifcation settings
    
    * Refactor notification item saving method and update NotificationEntity properties
    
    * Check settings before sending notifcation
    
    * Showing saved notifcation settings when editing settings
    
    * Showing message to users to login in to save settings & showign saved message

--- CoBySi.Pomodoro.Repository/Repositories/IUserSettingsRepository.cs
+++ CoBySi.Pomodoro.Repository/Repositories/IUserSettingsRepository.cs
@@ -1,10 +1,11 @@
 using CoBySi.Pomodoro.Repository.Models;
 
-namespace CoBySi.Pomodoro.Repository;
+namespace CoBySi.Pomodoro.Repository.Repositories;
 
 public interface IUserSettingsRepository
 {
     Task<UserPomodoroSettingsEntity?> GetUserPomodoroSettingsAsync(string userId, CancellationToken cancellationToken);
     Task SavePomodoroSettingsAsync(UserPomodoroSettingsEntity entity, CancellationToken cancellationToken);
-
+    Task<NotificationEntity?> GetUserNotificationSettingsAsync(string userId, CancellationToken cancellationToken);
+    Task SaveNotificationItemAsync(NotificationEntity notificationItem, CancellationToken cancellationToken);
 }
--- CoBySi.Pomodoro.Repository/settings/MongoCollection.cs
+++ CoBySi.Pomodoro.Repository/settings/MongoCollection.cs
@@ -4,4 +4,5 @@ public class MongoCollection
 {
     public string? PomodoroCollection { get; set; }
     public string? UserSettingsCollection { get; set; }
+    public string? NotificationSettingsCollection { get; set; }
 }
