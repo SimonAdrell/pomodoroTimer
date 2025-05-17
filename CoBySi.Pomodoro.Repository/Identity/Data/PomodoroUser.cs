946b8ad7a960634fbd9ae28c0503dbef06e15f68
Simon Adrell <simon.adrell@gmail.com>
2025-05-17 19:26:59 +0200

    Added created info on users

--- CoBySi.Pomodoro.Repository/Identity/Data/PomodoroUser.cs
+++ CoBySi.Pomodoro.Repository/Identity/Data/PomodoroUser.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Identity;
 
 namespace CoBySi.Pomodoro.Repository.Identity.Data;
 
-// Add profile data for application users by adding properties to the PomodoroUser class
 public class PomodoroUser : IdentityUser
 {
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
 
